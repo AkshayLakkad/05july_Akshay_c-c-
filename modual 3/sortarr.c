c77ba584387b6084b963217fca2ee534dd92dfaa
AkshayLakkad <109733428+AkshayLakkad@users.noreply.github.com>
2022-10-03 03:36:49 -0800

    Add files via upload

---
+++ modual 3/sortarr.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+	int arr[5],i,j,t;
+	printf("Enter the Elements \n");
+	for(i=0;i<5; i++)
+	{
+		scanf("%d",&arr[i]);
+		
+	}
+	for(i=0; i<5; i++)
+	{
+		for(j=i+1; j<5; j++)
+		{
+			if(arr[i]>arr[j]);
+			{
+				t=arr[i];
+				arr[i]=arr[j];
+				arr[j]=t;
+			}
+		}
+		
+	}
+		printf("arr sort alements is:");
+		for(i=0;i<5;i++)
+	{
+		printf("%d:",arr[i]);
+	}
+	return 0;
+}
